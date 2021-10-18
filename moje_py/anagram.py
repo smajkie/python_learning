c7bd6f25dd00a69b2c034042e71fca939c84736d
Sebastian Majkowski <sebastian.majkowski@intel.com>
2021-10-18 18:26:33 +0200

    Initial commit

---
+++ moje_py/anagram.py
@@ -0,0 +1,22 @@
+def anagram(slowo):
+
+    lista=[]
+    odwrotna=[]
+
+    for i in slowo:
+        lista.append(i)
+
+    for j in slowo[len(slowo)::-1]:
+        odwrotna.append(j)
+
+    if lista == odwrotna:
+        return True
+    else:
+        return False
+
+#input = input('Podaj slowo:')
+if anagram(input('Podaj')):
+    print(f'Tak  to anagram!!!')
+else:
+    print(f'Nieeeeeeee,  to nie jest anagram')
+
