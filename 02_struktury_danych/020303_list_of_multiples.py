ea2fcc74757e416daf39a4fc986e2fae45927d79
smajkie <smajko@wp.pl>
2023-11-05 22:37:25 +0100

    surface_learn

--- 02_struktury_danych/020303_list_of_multiples.py
+++ 02_struktury_danych/020303_list_of_multiples.py
@@ -6,7 +6,8 @@ def list_of_multiples (num, length):
 
 print(list_of_multiples(12,10))
 
-# indexerror: list assignment index out of range - to sie bierze z tego ze zainicjowana listan ie ma elementu, nie mozna wiec przypisac wartosci do elementu ktory nie istnieje.
+# indexerror: list assignment index out of range - to sie bierze z tego ze zainicjowana listan ie ma elementu, nie mozna wiec przypisac
+#  wartosci do elementu ktory nie istnieje.
 # dwa rozwiazania:
 # - uzywanie append czyli dopisywania do listy
 # - zainicjowanie pustej listy z x iloscia elementow
